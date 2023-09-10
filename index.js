a3205d34ad6a87372df549bf535e11b14bbd38ae
VictorOlv <victorolvdev@gmail.com>
2023-09-10 14:26:03 -0300

    criacao user

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
-const bycript = require('bcrypt')
+const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken');
+const User = require('./models/User');
+const { restart } = require('nodemon');
+
 const app = express()
 
 app.use(express.json())
@@ -15,24 +18,51 @@ app.post('/user/register', async (req,res)=>{
     const { name , password , email} = req.body
 
     if(!name){
-        return res.json({
+        return res.status(422).json({
             msg: 'O username é obrigatório'
         })
     }
     
     if(!password){
-        return res.json({
+        return res.status(422).json({
             msg: 'O password é obrigatório'
         })
     }
     
     if(!email){
-        return res.json({
+        return res.status(422).json({
             msg: 'O email é obrigatório'
         })
     }
 
-    res.send('askop')
+    const userExists =  await User.findOne({email: email})
+
+    if(userExists){
+        return res.status(422).json({
+            msg:'Email já registrado'
+        })
+    }
+
+    const salt = await bcrypt.genSalt(12);
+    const passwordHash =  await bcrypt.hash(password, salt)
+
+
+    const user = new User({
+        name,
+        email,
+        password: passwordHash
+    })
+    try{
+        await user.save()
+
+        res.status(201).json({
+            msg:'Usuário criado com sucesso .'
+        })
+    }catch(e){
+        res.status(500).json({
+            msg:e
+        })
+    }
 
 })
 
